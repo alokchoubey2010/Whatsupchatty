8b10631f6524f4f9045e828086aaf82ffef648f0
alokchoubey2010 <131374624+alokchoubey2010@users.noreply.github.com>
2024-09-27 08:33:33 +0530

    Released v-1.2.0
    Added chat functionality

---
+++ chat.js
@@ -0,0 +1,106 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBtNChab23CAv6HmiJnMmyKb5ALQSOIn4s",
+  authDomain: "group-chat-neuron-nerds.firebaseapp.com",
+  databaseURL: "https://group-chat-neuron-nerds-default-rtdb.firebaseio.com",
+  projectId: "group-chat-neuron-nerds",
+  storageBucket: "group-chat-neuron-nerds.appspot.com",
+  messagingSenderId: "900309631047",
+  appId: "1:900309631047:web:583c5dce923fd4d43ff162"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+function showCreateRoomForm(){
+  // window.location.assign("./chat.html?action=create-room")
+  confirm = confirm(`Do you want to create a room with ${localStorage.getItem("selectedContact")}?`)
+  if(confirm){
+    alert(`Room already exists !`)
+  }else{
+    alert("Operation cancelled by User!")
+  }
+}
+
+document.querySelector(".inputMessage").addEventListener("input", () => {
+  document.querySelector(".send").style.display = "block";
+})
+
+function sendMessage(){
+  input = document.querySelector(".inputMessage");
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      msg = input.value;
+      sender = user.displayName;
+      time = new Date();
+      saveMessage(msg,sender);
+      chatBox = document.querySelector(".chatBox");
+      chatBox.innerHTML = '';
+      retriveAndDisplayMessage();
+    } else {
+      alert(`An authentication error has occured make sure you are signed in!`)
+    }
+  });
+}
+
+document.querySelector(".name").innerHTML = localStorage.getItem("selectedContact");
+
+
+function saveMessage(messageText, senderName) {
+  currentRoom = localStorage.getItem("currentRoom");
+  const messagesRef = firebase.database().ref(`rooms/${currentRoom}/${messageText}`);
+  // Creating the message object
+  const messageData = {
+    message: messageText,
+    sender: senderName,
+    time: Date.now()
+  };
+  messagesRef.set(messageData);
+}
+
+
+function retriveAndDisplayMessage(){
+  currentRoom = localStorage.getItem("currentRoom");
+  messageRef = firebase.database().ref(`rooms/${currentRoom}`).orderByChild('time');
+  messageRef.once('value', (snapshot) => {
+  snapshot.forEach((childSnapshot) => {
+    const data = childSnapshot.val();
+    selectedContact = localStorage.getItem("selectedContact");
+    if(data.sender === selectedContact){
+      displaySend(data.message,"recieved",convertTimestampTo24HourFormat(data.time));
+    }else{
+      displaySend(data.message,"sent",convertTimestampTo24HourFormat(data.time));
+    }
+  });
+});
+}
+
+retriveAndDisplayMessage();
+
+
+function displaySend(msg,className,time){
+  div = document.createElement("div");
+  chatBox = document.querySelector(".chatBox");
+  div.className = 'holder';
+  p = document.createElement("p");
+  p.className = className;
+  sub = document.createElement("sub");
+  sub.innerHTML = time;
+  p.innerHTML = msg;
+  p.appendChild(sub);
+  div.appendChild(p);
+  chatBox.appendChild(div);
+  document.querySelector(".inputMessage").value = '';
+}
+
+
+function convertTimestampTo24HourFormat(timestamp) {
+  // Create a Date object from the timestamp
+  const date = new Date(timestamp);
+
+  // Use toLocaleTimeString to convert to 24-hour format
+  return date.toLocaleTimeString('en-GB', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false // Ensure 24-hour format
+  });
+}
--- home.js
+++ home.js
@@ -19,13 +19,14 @@ function fetchUsersFromFirebase() {
     snapshot.forEach((childSnapshot) => {
       const user = childSnapshot.val();
       createContact(user.name, user.email, user.profileImg);
+      createProfileNavDisplay(user.profileImg,user.name);
     });
   });
 }
 
 // Function to create a contact
 function createContact(name, about, profileImg) {
-  const contactsDiv = document.querySelector(".chatContainer");
+  const contactsDiv = document.querySelector(".contacts");
 
   const div = document.createElement("div");
   div.className = "contact";
@@ -51,6 +52,21 @@ function createContact(name, about, profileImg) {
   div.appendChild(state);
 
   contactsDiv.appendChild(div);
+
+  // Add click event listener to each contact div
+  div.onclick = function() {
+    localStorage.setItem('selectedContact', name); 
+    firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    name1 = user.displayName;
+    name2 = localStorage.getItem("selectedContact");
+    createChat(name1, name2);
+    window.location.assign("chat.html")
+  } else {
+    alert("An unexpected authentication error occured make sure you have signed in!")
+  }
+});
+  }
 }
 
 function createSubstring(string, maxLength) {
@@ -61,3 +77,109 @@ function createSubstring(string, maxLength) {
 }
 
 fetchUsersFromFirebase();
+
+// Function to create profile nav display
+function createProfileNavDisplay(profileImg, name) {
+  const profileHolder = document.querySelector(".profileHolder");
+  const profile = document.createElement("div");
+  profile.className = 'profile';
+
+  const img = document.createElement("img");
+  img.className = 'profileImgNav';
+  img.src = profileImg;
+  profile.appendChild(img);
+
+  const p = document.createElement("p");
+  p.className = "nameLabel";
+  p.innerText = createSubstring(name, 5);
+  profile.appendChild(p);
+
+  profileHolder.appendChild(profile);
+}
+
+function toLowerCaseNoSpaces(str) {
+  // Convert the string to lowercase and remove all spaces
+  return str.toLowerCase().replace(/\s+/g, '');
+}
+
+
+database = firebase.database();
+
+function createChat(user1, user2) {
+  // Sort the usernames alphabetically
+  let sortedUsers = [user1, user2].sort(); // This will make sure the room name is consistent
+  let strs = sortedUsers[0] + sortedUsers[1];
+  let chat = toLowerCaseNoSpaces(strs); // Use lowercase and remove spaces
+
+  localStorage.setItem("currentRoom", chat);
+
+  if (checkForNode(chat)) {
+    console.log("Room already exists.");
+  } else {
+    // Room doesn't exist, create a new one
+    database.ref(`rooms/${chat}/New Chat Started`).set({
+      message: "New Chat Started!",
+      time: Date.now()
+    });
+    console.log("New room created.");
+  }
+}
+
+
+function checkForNode(nodeName){
+  const dbRef = firebase.database().ref('rooms/' + nodeName);
+  dbRef.get().then((snapshot) => {
+    if (snapshot.exists()) {
+      return true;
+    } else {
+      return false;
+    }
+  }).catch((error) => {
+    alert("Error fetching node:", error);
+  });
+}
+
+function showOptions(){
+  settingsOption = document.querySelector(".settingsOption");
+  settingsOption.classList.add("active");
+}
+
+function autocompleteSearch() {
+  const searchInput = document.getElementById('searchInput').value;
+  const autocompleteList = document.querySelector('.contacts');
+  autocompleteList.innerHTML = '';
+  getNameArray((nameArray) => {
+  console.log(nameArray)
+  if (nameArray.includes(searchInput.toLowerCase())) {
+    card = createContact(searchInput);
+    autocompleteList.appendChild(card);
+  } else {
+    autocompleteList.innerHTML = "No Contacts Found! Refresh the page."
+  }
+});
+}
+
+
+// Function to get name data from Firebase and store it in an array
+function getNameArray(callback) {
+  // Reference to users in Firebase
+  const usersRef = database.ref('users');
+
+  // Create an empty array to store the names
+  let nameArray = [];
+
+  // Fetch the users from Firebase
+  usersRef.once('value', (snapshot) => {
+    const users = snapshot.val();
+
+    // Loop through the users object and extract the Name property
+    for (const key in users) {
+      if (users[key].name) {
+        nameArray.push(users[key].name.toLowerCase()); // Extracting the "Name" property
+      }
+    }
+
+    // Call the callback function with the nameArray
+    callback(nameArray);
+  });
+}
